0a7a4e24e7109604ce25e1c699502596da209852
Simplilearn GitHub <siddam.bharat@simplilearn.net>
2023-05-31 18:31:31 +0530

    second commit

--- src/main/java/com/git_demo1/git_demo1/App.java
+++ src/main/java/com/git_demo1/git_demo1/App.java
@@ -9,9 +9,7 @@ public class App
 	
 	public void addData(String data)
 	{
-		System.out.println("data"+data);
-		
-		if(data.equals("ravi"))
+		if(data.equals(Constant.DATA_TYPE))
 		{
 			System.out.println("demo");
 		}
---
+++ src/main/java/com/git_demo1/git_demo1/Constant.java
@@ -0,0 +1,5 @@
+package com.git_demo1.git_demo1;
+
+public class Constant {
+public static String DATA_TYPE="siva";
+}
